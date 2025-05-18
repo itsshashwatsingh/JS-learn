b4228dec78ec43ac8f101d2640f464262da9d533
itsshashwatsingh <shashwat98k@gmail.com>
2025-05-18 14:02:03 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,59 @@
+
+// alert("hello world") // this will not work in node js but in browser 
+
+console.log("shashwat"); console.log(3   // not ideal for readablity
+    +3
+);     
+
+//-------------------------------
+
+console.log("helloworld"); // for better readablity
+console.log(5+5);
+
+//--------------------------------
+ 
+//some examples
+
+let name = "shashwat"  //string datatype
+
+let age = 22 // int datatype
+
+let isloggedIn = false // boolean datatype
+
+//----------------------------------
+
+// these datatypes are automaically specified by javascript bases on the values stored in variables
+
+// number => range is 2 to the power of 53 
+// bigint for really big number (generally use in trading , streaming or social media platforms)
+// string => "ejdhj5#$464ikej" (can store any text value)
+// boolean => true/false
+// Null => standalone value (its a value that can be stored)
+// undefined => it represents an empty variable (whose value is not assigned yet)
+// symbol => to find unique components (mostly used in react)
+
+
+// object (we will get to this later in detail)
+
+//------------------------------------
+
+//typeof 
+
+console.log(typeof age);
+
+// output -> number (age is a number datatype)
+
+console.log(typeof null);
+
+// output -> object (null is an object datatype as it can be stored value)
+
+console.log(typeof undefined);
+
+// output -> undefined 
+
+
+
+
+
+
+
